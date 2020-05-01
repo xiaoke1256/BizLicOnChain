c79bf75e94a09e6c4deb119328b727144816fb91
tangjun <tangjun@CN-20170216CTDG>
2020-05-01 16:56:48 +0800

    deploy

--- BizLicOnChain/migrations/2_deploy_contracts.js
+++ BizLicOnChain/migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+//const ConvertLib = artifacts.require("ConvertLib");
+//const MetaCoin = artifacts.require("MetaCoin");
+const BizlicOnchain = artifacts.require("BizlicOnchain");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  //deployer.deploy(ConvertLib);
+  //deployer.link(ConvertLib, MetaCoin);
+  //deployer.deploy(MetaCoin);
+  deployer.deploy(BizlicOnchain);
 };
