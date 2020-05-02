b69aaa86d8a781f096091650ae101e298422fab0
tangjun <tangjun@CN-20170216CTDG>
2020-05-02 23:09:06 +0800

    Arrays Util

---
+++ BizLicOnChain/contracts/ArrayUtils.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.25;
+
+library ArrayUtils {
+	function contains(address[] array,address target) internal returns (bool){
+	    
+	}
+	
+	function contains(bytes[] array,string target) internal returns (bool){
+	    
+	}
+}
