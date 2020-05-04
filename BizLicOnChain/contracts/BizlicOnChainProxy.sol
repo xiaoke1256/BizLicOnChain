fe2d237f24e9c883d2c7cbbd5373e10f9e838aa0
tangjun <tangjun@CN-20170216CTDG>
2020-05-04 21:26:12 +0800

    合约版本升级方案.

--- BizLicOnChain/contracts/BizlicOnChainProxy.sol
+++ BizLicOnChain/contracts/BizlicOnChainProxy.sol
@@ -4,9 +4,40 @@ pragma solidity ^0.4.25;
  * 合约代理
  */
 contract BizLicOnChainProxy {
-    address creator;
-
-    constructor() public{
-        creator = msg.sender;
+    address owner;
+    
+     /*
+     * 管理员
+     */
+    address[] administrators;
+    
+    /**
+     * 真正的合约地址
+     */
+    address public currentVersion;
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
     }
+	
+	/**
+	 * 创建合约
+	 */
+    constructor(address initAddr) public{
+        owner = msg.sender;
+        currentVersion = initAddr;
+    }
+    
+    /**
+     * 合约版本变更
+     */
+    function changeContract(address newVersion) public onlyOwner{
+        currentVersion = newVersion;
+    }
+    
+    function() public {
+        require(currentVersion.delegatecall(msg.data));
+    }
+    
 }
--- BizLicOnChain/migrations/2_deploy_contracts.js
+++ BizLicOnChain/migrations/2_deploy_contracts.js
@@ -2,10 +2,12 @@
 //const MetaCoin = artifacts.require("MetaCoin");
 const ArrayUtils = artifacts.require("ArrayUtils");
 const BizlicOnchain = artifacts.require("BizlicOnchain");
+const BizLicOnChainProxy = artifacts.require("BizLicOnChainProxy");
 
 module.exports = function(deployer) {
   deployer.deploy(ArrayUtils);
   deployer.link(ArrayUtils, BizlicOnchain);
   //deployer.deploy(MetaCoin);
   deployer.deploy(BizlicOnchain);
+  deployer.deploy(BizLicOnChainProxy);
 };
