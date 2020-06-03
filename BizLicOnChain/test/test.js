cfe52a5277db92327f89c558e289cf02a2f636ab
tangjun <tangjun@CN-20170216CTDG>
2020-06-03 19:05:54 +0800

    单元测试.

--- BizLicOnChain/test/test.js
+++ BizLicOnChain/test/test.js
@@ -5,12 +5,13 @@ contract('EncryptUtils Test', function(accounts){
     return EncryptUtils.deployed().then(async function(instance){
     	let licContent = "{uniScId:'123100008654367537',corpName:'某某股份有限公司',leadName:'李四',indsyCode:'6432',regCpt:1250000.00,provDate:'2020-05-26',limitTo:'2025-05-26',issueOrgan:'上海市市场监督局'}";
     	let sign = await web3.eth.accounts.sign(licContent,'0x785154a70d49e368e64c82fd457917905ad2e70e4aded78b5424ebe7b34a00bd');
+    	console.log(typeof( sign.signature));
     	console.log(sign.signature);
     	console.log(sign.signature.length);
-    	let r = await instance.slice(sign.signature,0,64);
+    	let r = await instance.slice(sign.signature,0,32);
     	console.log(r);
     	console.log(r.length);
-    	let s = await instance.slice(sign.signature,64,64);
+    	let s = await instance.slice(sign.signature,32,32);
     	console.log(s);
     	let res = await instance.verificate(licContent,sign.signature,'0xa468fb3adf2c5d7e19b6ba353862b7b195027b2b');
     	console.log(res.valueOf());
