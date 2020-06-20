82953f10778cce026ab98a6783b09754e64c30a8
tangjun <tangjun@CN-20170216CTDG>
2020-06-20 23:25:17 +0800

    json 符号错误。

--- BizLicOnChain/truffle-config.js
+++ BizLicOnChain/truffle-config.js
@@ -55,11 +55,11 @@ module.exports = {
     
     // My network 
     MyNetwork: {
-      host: "192.168.66.100";
+      host: "192.168.66.100",
       port: 30303,             // Custom port
       network_id: 123,       // Custom network
       gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+      gasPrice: 20000000000   // 20 gwei (in wei) (default: 100 gwei)
       //from: <address>,        // Account to send txs from (default: accounts[0])
       // websockets: true        // Enable EventEmitter interface for web3 (default: false)
     },
