ad19e9810ebf1202cd3d887f073e9b2523c470ae
tangjun <tangjun@CN-20170216CTDG>
2020-07-07 21:58:13 +0800

    ip

--- BizLicOnChainCli/src/com/xiaoke1256/bizliconchain/blockchain/cli/BizLicOnChainCli.java
+++ BizLicOnChainCli/src/com/xiaoke1256/bizliconchain/blockchain/cli/BizLicOnChainCli.java
@@ -1,6 +1,9 @@
 package com.xiaoke1256.bizliconchain.blockchain.cli;
 
 public class BizLicOnChainCli {
-
-	//RawTransaction rawTransaction = RawTransaction.createContractTransaction();
+	public void sendLic(String dataJson) {
+		String url = "http://127.0.0.1:8545";
+		//HttpClient
+	}
+	
 }
