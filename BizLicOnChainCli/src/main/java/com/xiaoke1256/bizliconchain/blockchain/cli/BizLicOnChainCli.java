d31d9959f6eb828d7921ef910f1385739ce09165
tangjun <tangjun@CN-20170216CTDG>
2020-07-11 00:05:44 +0800

    http client

--- BizLicOnChainCli/src/main/java/com/xiaoke1256/bizliconchain/blockchain/cli/BizLicOnChainCli.java
+++ BizLicOnChainCli/src/main/java/com/xiaoke1256/bizliconchain/blockchain/cli/BizLicOnChainCli.java
@@ -1,9 +1,27 @@
 package com.xiaoke1256.bizliconchain.blockchain.cli;
 
+import java.io.IOException;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.protocol.HTTP;
+
 public class BizLicOnChainCli {
+	private HttpClient httpClient = HttpClients.createDefault();
+	
+	private String url = "http://127.0.0.1:8545";
+	
 	public void sendLic(String dataJson) {
-		String url = "http://127.0.0.1:8545";
-		//HttpClient
+		HttpPost request = new HttpPost(url);
+		 
+		try {
+			request.setEntity(new StringEntity("",HTTP.UTF_8));
+			httpClient.execute(request );
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
 }
