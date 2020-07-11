5e22bce1d5ca3e7d7613548b3f0f3b4d4460a474
tangjun <tangjun@CN-20170216CTDG>
2020-07-11 16:28:41 +0800

    客户端调用.

--- BizLicOnChainCli/src/main/java/com/xiaoke1256/bizliconchain/blockchain/cli/BizLicOnChainCli.java
+++ BizLicOnChainCli/src/main/java/com/xiaoke1256/bizliconchain/blockchain/cli/BizLicOnChainCli.java
@@ -1,6 +1,9 @@
 package com.xiaoke1256.bizliconchain.blockchain.cli;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Random;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
@@ -13,7 +16,15 @@ public class BizLicOnChainCli {
 	
 	private String url = "http://127.0.0.1:8545";
 	
-	public void sendLic(String dataJson) {
+	private int clientId = new Random(10000).nextInt();
+
+	public void sendLic(String uniScId,String organCode,String licContent,String sign) {
+		Map<String,Object> sendData = new LinkedHashMap<String,Object>();
+		sendData.put("id",Integer.valueOf(clientId));
+		sendData.put("jsonrpc","2.0");
+		sendData.put("method","eth_sendTransaction");
+		sendData.put("params", "[{}]");
+		
 		HttpPost request = new HttpPost(url);
 		 
 		try {
