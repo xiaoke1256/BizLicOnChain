b442dbc2385e3457ed82eb6ba7bcab4d4dc361ec
tangjun <tangjun@CN-20170216CTDG>
2020-07-23 18:23:07 +0800

    获取账户.

--- BizLicOnChain/test/private_key.js
+++ BizLicOnChain/test/private_key.js
@@ -1,6 +1,6 @@
 var keythereum = require("keythereum");  
-var datadir = "/Users/guanhongchang/testNet"; //data存放的地址路径
-var address= "0x886aeec3425061ee24928f5b0f3745d051948caa";//要小写  
+var datadir = "C:\Program Files\Geth\data"; //data存放的地址路径
+var address= "aefe184653e506426daab8369504ce3a2d95b68c";//要小写  
 const password = "1234567890"; //账户密码
 var keyObject = keythereum.importFromFile(address, datadir);  
 var privateKey = keythereum.recover(password, keyObject);  
