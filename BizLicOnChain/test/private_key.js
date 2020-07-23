45ece69e98a211fa7815c927e7e174c71f0a613c
tangjun <tangjun@CN-20170216CTDG>
2020-07-23 22:28:59 +0800

    keystore path.

--- BizLicOnChain/test/private_key.js
+++ BizLicOnChain/test/private_key.js
@@ -1,5 +1,5 @@
 var keythereum = require("keythereum");  
-var datadir = "C:\Program Files\Geth\data"; //data存放的地址路径
+var datadir = "C:\\Program Files\\Geth\\data"; //data存放的地址路径
 var address= "aefe184653e506426daab8369504ce3a2d95b68c";//要小写  
 const password = "1234567890"; //账户密码
 var keyObject = keythereum.importFromFile(address, datadir);  
