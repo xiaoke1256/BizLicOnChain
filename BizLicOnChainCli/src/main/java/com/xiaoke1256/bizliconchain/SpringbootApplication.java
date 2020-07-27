3af3ea6866df7276cf8fbf46cfad3b434f8563d3
tangjun <tangjun@CN-20170216CTDG>
2020-07-27 23:27:06 +0800

    start unit test.

--- BizLicOnChainCli/src/main/java/com/xiaoke1256/bizliconchain/SpringbootApplication.java
+++ BizLicOnChainCli/src/main/java/com/xiaoke1256/bizliconchain/SpringbootApplication.java
@@ -2,9 +2,11 @@ package com.xiaoke1256.bizliconchain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+@SpringBootApplication
 @EnableAutoConfiguration
 public class SpringbootApplication extends SpringBootServletInitializer {
 	
