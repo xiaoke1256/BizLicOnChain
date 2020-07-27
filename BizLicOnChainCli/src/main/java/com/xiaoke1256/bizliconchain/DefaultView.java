5d28d604f539bebbcf120eeb3817630d53c60ba4
tangjun <tangjun@CN-20170216CTDG>
2020-07-27 23:17:33 +0800

    web config.

---
+++ BizLicOnChainCli/src/main/java/com/xiaoke1256/bizliconchain/DefaultView.java
@@ -0,0 +1,16 @@
+package com.xiaoke1256.bizliconchain;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class DefaultView extends WebMvcConfigurerAdapter{
+    @Override
+    public void addViewControllers( ViewControllerRegistry registry ) {
+        registry.addViewController( "/" ).setViewName( "forward:/index.html" );
+        registry.setOrder( Ordered.HIGHEST_PRECEDENCE );
+        super.addViewControllers( registry );
+    } 
+}
