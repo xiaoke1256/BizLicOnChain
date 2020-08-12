ca3d949e24ca7467d724837f6f2fe2c7faa62f5d
tangjun <tangjun@CN-20170216CTDG>
2020-08-12 23:02:15 +0800

    return type error

--- BizLicOnChainCli/src/main/java/com/xiaoke1256/bizliconchain/common/web3j/cli/BaseWeb3jImpl.java
+++ BizLicOnChainCli/src/main/java/com/xiaoke1256/bizliconchain/common/web3j/cli/BaseWeb3jImpl.java
@@ -110,25 +110,4 @@ public class BaseWeb3jImpl implements IBaseWeb3j {
 
         return hash;
     }
-    
-    public BlockchainTransaction process(BlockchainTransaction trx) throws IOException {
-        EthAccounts accounts = web3j.ethAccounts().send();
-        EthGetTransactionCount transactionCount = web3j.ethGetTransactionCount(accounts.getAccounts().get(trx.getFromId()), DefaultBlockParameterName.LATEST).send();
-        Transaction transaction = Transaction.createEtherTransaction(accounts.getAccounts().get(trx.getFromId()), transactionCount.getTransactionCount(), BigInteger.valueOf(trx.getValue()), BigInteger.valueOf(21_000), accounts.getAccounts().get(trx.getToId()),BigInteger.valueOf(trx.getValue()));
-        EthSendTransaction response = web3j.ethSendTransaction(transaction).send();
-        if (response.getError() != null) {
-            trx.setAccepted(false);
-            return trx;
-        }
-        trx.setAccepted(true);
-        String txHash = response.getTransactionHash();
-        LOGGER.info("Tx hash: {}", txHash);
-        trx.setId(txHash);
-        EthGetTransactionReceipt receipt = web3j.ethGetTransactionReceipt(txHash).send();
-        if (receipt.getTransactionReceipt().isPresent()) {
-            LOGGER.info("Tx receipt: {}", receipt.getTransactionReceipt().get().getCumulativeGasUsed().intValue());
-        }
-        return trx;
-    }
- 
 }
