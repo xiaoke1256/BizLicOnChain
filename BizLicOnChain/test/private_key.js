69821d3753b7cfc7b0ff3f086c2552bbcaa36efa
tangjun <tangjun@CN-20170216CTDG>
2020-08-19 22:30:01 +0800

    get private key

--- BizLicOnChain/test/private_key.js
+++ BizLicOnChain/test/private_key.js
@@ -1,7 +1,7 @@
 var keythereum = require("keythereum");  
-var datadir = "C:\\Program Files\\Geth\\data"; //data存放的地址路径
-var address= "aefe184653e506426daab8369504ce3a2d95b68c";//要小写  
-const password = "1234567890"; //账户密码
+var datadir = "C:\\Program Files\\Geth\\data1"; //data存放的地址路径
+var address= "456247d681c799bf184085c2e1db8409add60c34";//要小写  
+const password = "123456"; //账户密码
 var keyObject = keythereum.importFromFile(address, datadir);  
 var privateKey = keythereum.recover(password, keyObject);  
 
