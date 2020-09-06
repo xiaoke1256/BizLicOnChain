b172dc7948d2dab8b6532d7d964b8c60199d732c
tangjun <tangjun@CN-20170216CTDG>
2020-09-06 11:24:31 +0800

    submit to back end.

---
+++ BizLicOnChainCli/src/main/java/com/xiaoke1256/bizliconchain/common/mvc/RespMsg.java
@@ -0,0 +1,25 @@
+package com.xiaoke1256.bizliconchain.common.mvc;
+
+public class RespMsg {
+	private String code;
+	private String msg;
+	
+	public RespMsg(String code, String msg) {
+		super();
+		this.code = code;
+		this.msg = msg;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+}
