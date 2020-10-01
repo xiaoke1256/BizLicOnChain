18ec62ccadb1e7e0a6f1760ab7d9a9393a7b2f7e
tangjun <tangjun@CN-20170216CTDG>
2020-10-01 22:51:15 +0800

    separate the organ management.

--- BizLicOnChain/contracts/AicOrgansHolder.sol
+++ BizLicOnChain/contracts/AicOrgansHolder.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.6.0;
 
+import { ArrayUtils } from "./ArrayUtils.sol";
 import { BaseAicOrgansHolder } from "./BaseAicOrgansHolder.sol";
 
 contract AicOrgansHolder is BaseAicOrgansHolder {
@@ -8,8 +9,83 @@ contract AicOrgansHolder is BaseAicOrgansHolder {
         creator = msg.sender;
     }
 
-    // TODO Add functions
+     /**
+     * 初始化合约(用delegatecall来调用)
+     */
+    function initialize() external {
+        require(creator == tx.origin);
+        administrators.push(tx.origin);
+    }
     
-
+	/**
+	 * 将老版本数据加载过来
+	 */
+	function reloadData(address oldVersion) public {
+	    //.....
+	}
+    
+    /**
+     * 仅管理员才可以执行此
+     */
+    modifier onlyAdmin() {
+        //tx.origin 是合约的发起方，而msg.sender是上一级调用者的地址
+		require(ArrayUtils.contains(administrators,tx.origin),"Unauthorized operation!");
+		_;
+    }
+    
+    //以下业务函数
+    /**
+     * 添加一个管理员
+     */
+    function addAdmin(address admin) public onlyAdmin returns (bool) {
+        if(!ArrayUtils.contains(administrators,admin)){
+            administrators.push(admin);
+        }
+        return true;
+    }
+    
+     /**
+     * 删除一个管理员
+     */
+    function removeAdmin(address admin) public onlyAdmin returns (bool) {
+       // 从 administrators中删除指定的管理员
+       require(administrators.length>1,"At least one administrator exist in this System!");
+       ArrayUtils.remove(administrators,admin);
+       return true;
+    }
+    
+    /**
+     * 获取所有的管理员
+     */
+    function getAdmins() public view returns(address[] memory){
+        return administrators;
+    }
+	
+	    
+    /**
+     * 注册一个发证机关
+     */
+    function regestOrgan(string memory organCode,string memory organName,address publicKey) public onlyAdmin returns (bool) {
+        //检查名称有没有，code有没有
+        require(bytes(organCode).length>0);
+        require(bytes(organName).length>0);
+        aicOrgans[organCode].organCode = organCode;
+        aicOrgans[organCode].organName = organName;
+        aicOrgans[organCode].publicKey = publicKey;
+        aicOrgans[organCode].isUserd = true;
+        
+        aicOrganCodes.push(organCode);
+        return true;
+    }
+    
+    /**
+     * 删除一个发证机关
+     */
+    function removeOrgan(string memory organCode) public onlyAdmin returns (bool) {
+        require(bytes(organCode).length>0);
+        delete aicOrgans[organCode];
+        ArrayUtils.remove(aicOrganCodes,organCode);
+        return true;
+    }
 
 }
--- BizLicOnChain/contracts/AicOrgansHolderProxy.sol
+++ BizLicOnChain/contracts/AicOrgansHolderProxy.sol
@@ -2,13 +2,160 @@ pragma solidity ^0.6.0;
 
 import { BaseAicOrgansHolder } from "./BaseAicOrgansHolder.sol";
 
+import { StringUtils } from "./StringUtils.sol";
+
 contract AicOrgansHolderProxy is BaseAicOrgansHolder {
     
+    bool internal _initialized = false;
+    
+    /**
+     * 逻辑合约地址
+     */
+    address currentVersion;
+    
+    modifier onlyCreator() {
+        require(msg.sender == creator);
+        _;
+    }
+	
+	/**
+	 * 创建合约
+	 */
     constructor() public{
         creator = msg.sender;
     }
+    
+    /**
+     * 初始化合约
+     */
+    function initialize(address newVersion) public onlyCreator{
+        require(!_initialized);
+        currentVersion = newVersion;
+        bool sucess;
+        (sucess,)= currentVersion.delegatecall(abi.encodeWithSignature("initialize()"));
+        require(sucess,'Fail to execute initialize function.');//初始化合约
+        _initialized = true;
+    }
+    
+    /**
+     * 合约版本变更
+     */
+    function changeContract(address newVersion) public onlyCreator{
+        require(_initialized);
+        currentVersion = newVersion;
+    }
+    
+    /**
+     * 将老版本数据加载过来
+     */
+    function reloadData(address oldVersion) public onlyCreator{
+        require(_initialized);
+		bool sucess;
+        (sucess,)= currentVersion.delegatecall(abi.encodeWithSignature("reloadData(address)",oldVersion));
+        require(sucess);
+    }
+    
+     //以下业务函数
+    /**
+     * 添加一个管理员
+     */
+    function addAdmin(address admin) public returns(bool) {
+        require(_initialized);
+        bool sucess;
+        bytes memory result;
+        (sucess,result)= currentVersion.delegatecall(abi.encodeWithSignature("addAdmin(address)",admin));
+        return (sucess && bytesToBool(result));
+    }
+    
+     /**
+     * 删除一个管理员
+     */
+    function removeAdmin(address admin) public returns(bool){
+       require(_initialized);
+       bool sucess;
+       bytes memory result;
+       (sucess,result)= currentVersion.delegatecall(abi.encodeWithSignature("removeAdmin(address)",admin));
+       return (sucess && bytesToBool(result));
+    }
+    
+    /**
+     * 获取所有的管理员
+     */
+    function getAdmins() public view returns(address[] memory admins){
+        require(_initialized);
+        return administrators;//BizlicOnChain(currentVersion).getAdmins();
+    }
+    
+    /**
+     * 注册一个发证机关
+     */
+    function regestOrgan(string memory organCode,string memory organName,address publicKey) public returns(bool){
+        require(_initialized);
+        bool sucess;
+        bytes memory result;
+        (sucess,result)= currentVersion.delegatecall(abi.encodeWithSignature("regestOrgan(string,string,address)",organCode,organName,publicKey));
+        return (sucess && bytesToBool(result));
+    }
+    
+    function removeOrgan(string memory organCode)public returns(bool) {
+        require(_initialized);
+        bool sucess;
+        bytes memory result;
+        (sucess,result)= currentVersion.delegatecall(abi.encodeWithSignature("removeOrgan(string)",organCode));
+        return (sucess && bytesToBool(result));
+    }
+    
+    
+    /**
+     * 获取所有发证机关
+     */
+    function getOrgan(string memory organCode) public view returns(string memory) {
+        require(_initialized);
+        if(!aicOrgans[organCode].isUserd){
+            return "null";
+        }
+        string[] memory strArr = new string[](7);
+        strArr[0]="{organCode:'";
+        strArr[1]=organCode;
+        strArr[2]="',organName:'";
+        strArr[3]=aicOrgans[organCode].organName;
+        strArr[4]="',publicKey:'";
+        strArr[5]=StringUtils.address2str(aicOrgans[organCode].publicKey);
+        strArr[6]="'}";
+        return StringUtils.concat(strArr);
+    }
+    
+    /**
+     * 获取所有的发证机关
+     */
+    function getAllOrganCodes() public view returns(string memory){
+        string memory s = '[';
+        for(uint64 i = 0;i<aicOrganCodes.length;i++){
+            if(i>0){
+                s=StringUtils.concat(s,",");
+            }
+            s=StringUtils.concat(s,"'",aicOrganCodes[i],"'");
+        }
+        s=StringUtils.concat(s,"]");
+        return s;
+    }
 
-    // TODO Add functions
-
+	 /**
+     * 把字节数组转成布尔型
+     */
+    function bytesToBool(bytes memory b) private pure returns(bool){
+        return bytesToUint(b)!=0;
+    }
+    
+    /**
+     * 把字节数组转成整数
+     */
+    function bytesToUint(bytes memory b) private pure returns (uint8){
+	    uint8 number = 0;
+	    for(uint64 i= 0; i<b.length; i++){
+	        number = uint8(number + uint8(b[i])*(2**(8*(b.length-(i+1)))));
+	    }
+	    return number;
+	}
 
 }
