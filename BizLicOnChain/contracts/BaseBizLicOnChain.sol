3b94d6709fd844261b3993d2e7b7cd8e676a8ebc
xiaoke_1256 <xiaoke_1256@sina.com>
2020-12-13 21:44:25 +0800

    remove AicOrgan in lic contract.

--- BizLicOnChain/contracts/BaseBizLicOnChain.sol
+++ BizLicOnChain/contracts/BaseBizLicOnChain.sol
@@ -4,15 +4,6 @@ pragma solidity ^0.6.0;
  * 营业执照电子合约的基类，用来定义要用到的结构体
  */
 contract BaseBizLicOnChain {
-    /**
-     * 工商局
-     */
-    struct AicOrgan{
-        string organCode;//机关代码
-        string organName;//机关名称
-        address publicKey;//公钥
-        bool isUserd;
-    }
     
     /**
      * 营业执照
