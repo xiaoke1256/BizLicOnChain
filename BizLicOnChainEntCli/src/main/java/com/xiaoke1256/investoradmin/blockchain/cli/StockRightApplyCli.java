72cd543b911fa944f3751e8d40429a89fe26b560
xiaoke_1256 <xiaoke_1256@sina.com>
2022-04-16 18:04:37 +0800

    客户端

---
+++ BizLicOnChainEntCli/src/main/java/com/xiaoke1256/investoradmin/blockchain/cli/StockRightApplyCli.java
@@ -0,0 +1,8 @@
+package com.xiaoke1256.investoradmin.blockchain.cli;
+
+public class StockRightApplyCli {
+/*
+	function startStockTransfer(string memory uniScId,string memory transferorCetfHash,string memory investorName,
+			string memory investorCetfHash,bytes32 merkel,uint cptAmt,uint price)public returns (bool){
+			*/
+}
