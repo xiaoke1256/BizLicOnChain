e9c16a206da4d1df5d0abd0ae46d0fcf2f1a4204
xiaoke_1256 <xiaoke_1256@sina.com>
2022-05-02 09:35:06 +0800

    注册事件

--- BizLicOnChainCli-common/src/main/java/com/xiaoke1256/bizliconchain/common/web3j/cli/BaseWeb3jImpl.java
+++ BizLicOnChainCli-common/src/main/java/com/xiaoke1256/bizliconchain/common/web3j/cli/BaseWeb3jImpl.java
@@ -5,6 +5,8 @@ import com.xiaoke1256.bizliconchain.common.bo.EthTrasLog;
 import com.xiaoke1256.bizliconchain.common.service.EthTrasLogService;
 import com.xiaoke1256.bizliconchain.common.util.HexUtil;
 
+import io.reactivex.functions.Consumer;
+
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,6 +29,7 @@ import org.web3j.protocol.core.methods.response.EthCall;
 import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
 import org.web3j.protocol.core.methods.response.EthSendTransaction;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.protocol.websocket.events.LogNotification;
 import org.web3j.utils.Numeric;
 
 import java.io.IOException;
@@ -249,6 +252,17 @@ public class BaseWeb3jImpl implements IBaseWeb3j {
     	return web3j.ethGetBalance(address, DefaultBlockParameterName.LATEST).send().getBalance();
 		
 	}
+    
+    /**
+     * 注册事件监听
+     * @param contractAddress
+     * @param topic 类似 "ApplyStatusChange(string,string,string)"
+     * @param onSuccess
+     * @param onError
+     */
+    public void subscript(String contractAddress,String topic ,Consumer<LogNotification> onSuccess,Consumer<Throwable> onError) {
+    	web3j.logsNotifications(Arrays.asList(contractAddress), Arrays.asList(topic)).blockingSubscribe(onSuccess, onError);
+    }
     
     
 }
