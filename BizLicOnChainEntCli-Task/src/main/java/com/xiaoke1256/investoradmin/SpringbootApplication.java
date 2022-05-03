6c684b5d5812271dc846140c56e76fbff2f61511
xiaoke_1256 <xiaoke_1256@sina.com>
2022-05-03 22:11:53 +0800

    证明了专门做个task应用不靠谱

--- BizLicOnChainEntCli-Task/src/main/java/com/xiaoke1256/investoradmin/SpringbootApplication.java
+++ BizLicOnChainEntCli-Task/src/main/java/com/xiaoke1256/investoradmin/SpringbootApplication.java
@@ -1,18 +1,18 @@
 package com.xiaoke1256.investoradmin;
 
-//import org.mybatis.spring.annotation.MapperScan;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 //@EnableScheduling
-//@SpringBootApplication(scanBasePackages= {"com.xiaoke1256.bizliconchain","com.xiaoke1256.investoradmin"},exclude={HibernateJpaAutoConfiguration.class})
-//@EnableAutoConfiguration
-//@MapperScan("com.xiaoke1256.**.dao")
+@SpringBootApplication(scanBasePackages= {"com.xiaoke1256.bizliconchain","com.xiaoke1256.investoradmin"},exclude={HibernateJpaAutoConfiguration.class})
+@EnableAutoConfiguration
+@MapperScan("com.xiaoke1256.**.dao")
 public class SpringbootApplication {
 	public static void main(String[] args) {
-        //SpringApplication.run(SpringbootApplication.class, args);
+        SpringApplication.run(SpringbootApplication.class, args);
     }
 	
 	
---
+++ BizLicOnChainEntCli-Task/src/main/java/com/xiaoke1256/investoradmin/eventslistener/ListenerRegistor.java
@@ -0,0 +1,13 @@
+package com.xiaoke1256.investoradmin.eventslistener;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 事件监听注册器
+ * @author TangJun
+ *
+ */
+@Component
+public class ListenerRegistor {
+
+}
