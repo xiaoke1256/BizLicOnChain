f5dabb1e42947a5774331e3b7f6de5d2adce16c0
xiaoke_1256 <xiaoke_1256@sina.com>
2022-05-03 21:13:10 +0800

    task项目

---
+++ BizLicOnChainEntCli-Task/src/main/java/com/xiaoke1256/investoradmin/SpringbootApplication.java
@@ -0,0 +1,19 @@
+package com.xiaoke1256.investoradmin;
+
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+//@EnableScheduling
+//@SpringBootApplication(scanBasePackages= {"com.xiaoke1256.bizliconchain","com.xiaoke1256.investoradmin"},exclude={HibernateJpaAutoConfiguration.class})
+//@EnableAutoConfiguration
+//@MapperScan("com.xiaoke1256.**.dao")
+public class SpringbootApplication {
+	public static void main(String[] args) {
+        //SpringApplication.run(SpringbootApplication.class, args);
+    }
+	
+	
+}
