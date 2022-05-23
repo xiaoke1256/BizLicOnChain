06239e1a527fc2fdd38db93fa32dfe356c5e0b74
xiaoke_1256 <xiaoke_1256@sina.com>
2022-05-23 18:13:22 +0800

    readOnly

--- BizLicOnChainCli/src/main/java/com/xiaoke1256/bizliconchain/blockchain/cli/StockRightApplyClient.java
+++ BizLicOnChainCli/src/main/java/com/xiaoke1256/bizliconchain/blockchain/cli/StockRightApplyClient.java
@@ -3,6 +3,7 @@ package com.xiaoke1256.bizliconchain.blockchain.cli;
 import java.util.List;
 
 import com.xiaoke1256.bizliconchain.blockchain.common.client.annotation.EthClient;
+import com.xiaoke1256.bizliconchain.blockchain.common.client.annotation.ReadOnly;
 import com.xiaoke1256.bizliconchain.bo.StockRightApply;
 
 @EthClient(contractAddress="${contract.applyCtAddr}")
@@ -13,8 +14,10 @@ public interface StockRightApplyClient {
 	 * @param investorCetfHash
 	 * @return
 	 */
+	@ReadOnly
 	public List<String> getStockRightApplyKeysByUniScId(String uniScId);
 	
+	@ReadOnly
 	public StockRightApply getStockRightApply(String uniScId,String investorCetfHash);
 	
 	public void backUp(String uniScId,String investorCetfHash,boolean isPass,String reason);
