ed549df2f886b47abebe2c4c75290b63abff724d
xiaoke_1256 <xiaoke_1256@sina.com>
2022-05-25 20:25:45 +0800

    缺扫描配置

--- BizLicOnChainEntCli/src/main/java/com/xiaoke1256/investoradmin/SpringbootApplication.java
+++ BizLicOnChainEntCli/src/main/java/com/xiaoke1256/investoradmin/SpringbootApplication.java
@@ -9,10 +9,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import com.xiaoke1256.bizliconchain.blockchain.common.client.annotation.EthClientScan;
+
 @EnableScheduling
 @SpringBootApplication(scanBasePackages= {"com.xiaoke1256.bizliconchain","com.xiaoke1256.investoradmin"},exclude={HibernateJpaAutoConfiguration.class})
 @EnableAutoConfiguration
 @MapperScan("com.xiaoke1256.**.dao")
+@EthClientScan("com.xiaoke1256.investoradmin.blockchain.cli")
 public class SpringbootApplication extends SpringBootServletInitializer {
 	
 	@Override
